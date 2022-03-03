e0a5b6366beb7cc3c7b06604bb6cf155097dcfb1
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-03-03 12:45:44 +0300

    Add routes to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PrivateOutlet from "./auth/PrivateOutlet";
 import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
 import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<PrivateOutlet />}>
+          <Route path="/" element={<HomePage />} />
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
