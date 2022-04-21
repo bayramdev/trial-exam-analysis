21564a4a4377c910a5fd06cbe89e7427db08f741
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-04-21 15:34:01 +0300

    Update useAuth user response typings

--- src/auth/useAuth.ts
+++ src/auth/useAuth.ts
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import axios from "axios";
 import { BASE_URL } from "../config";
-import { LoginCredentials, LoginResponse, User } from "../types";
+import { LoginCredentials, LoginResponse, User, UserResponse } from "../types";
 import { useToken } from "./useToken";
 
-const reqUser = async (token: string): Promise<User> => {
+const reqUser = async (token: string): Promise<UserResponse> => {
   const res = await axios.get("me", {
     baseURL: BASE_URL,
     headers: { Authorization: `Bearer ${token}` },
@@ -26,17 +26,27 @@ export const useAuth = () => {
     cachedUser,
 
     loginUser: async (cred: LoginCredentials) => {
-      const { user, token } = await reqLogIn(cred);
-      setCachedUser(user);
-      setToken(token);
+      const { user, token, success } = await reqLogIn(cred);
+      if (!success) return;
+
+      if (user && token) {
+        setCachedUser(user);
+        setToken(token);
+      }
+
       return user;
     },
 
     fetchUser: async () => {
-      if (!token) return null;
-      const userRes = await reqUser(token);
-      setCachedUser(userRes);
-      return userRes;
+      if (!token) return;
+      const { user, success } = await reqUser(token);
+      if (success) return;
+
+      if (user) {
+        setCachedUser(user);
+      }
+
+      return user;
     },
 
     logOut: async () => {
