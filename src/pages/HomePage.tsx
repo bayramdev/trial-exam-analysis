204a51266c4c412305797128b5fae8c8050d56b1
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-04-21 14:56:33 +0300

    Console log cached user in homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,8 @@
+import { useAuth } from "../auth/useAuth";
+
 const HomePage = () => {
+  const { cachedUser } = useAuth();
+  console.log(cachedUser);
   return <div className="text-lg text-red-600">home page</div>;
 };
 
