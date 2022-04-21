4b35c057f4255646eb9bed77bde8601ea562b80d
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-04-21 15:49:11 +0300

    Install react-chartjs-2 and chart.js

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,29 @@
 import { useAuth } from "../auth/useAuth";
+import { Line } from "react-chartjs-2";
+
+const data = {
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+  datasets: [
+    {
+      label: "First dataset",
+      data: [33, 53, 85, 41, 44, 65],
+      fill: true,
+      backgroundColor: "rgba(75,192,192,0.2)",
+      borderColor: "rgba(75,192,192,1)",
+    },
+    {
+      label: "Second dataset",
+      data: [33, 25, 35, 51, 54, 76],
+      fill: false,
+      borderColor: "#742774",
+    },
+  ],
+};
 
 const HomePage = () => {
   const { cachedUser } = useAuth();
   console.log(cachedUser);
-  return <div className="text-lg text-red-600">home page</div>;
+  return <Line data={data} />;
 };
 
 export default HomePage;
